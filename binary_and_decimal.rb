d6eebaf867e5106619c5f902b83e6bde2e07371c
Sarah Read-Brown <srbusiness2@gmail.com>
2017-08-29 22:14:09 -0700

    Adds array_equals method logic

--- binary_and_decimal.rb
+++ binary_and_decimal.rb
@@ -1,19 +1,17 @@
 require 'pry'
-
 # 1. Write a method named `binary_to_decimal` that creates an array of size 8.
 #    Fill the array randomly with 0’s and 1’s. Print out the array so that it
 #    appears as a binary number. Calculate the decimal value for this binary
 #    number using the algorithm you devised in class. Print out the decimal value.
 def binary_to_decimal
   arr = Array.new(8) {rand(0..1)}
-  b_string = arr.join('')
   arr.reverse!
 
-  dec_number = 0
+  total = 0
   arr.each_with_index do | num, i|
     total += (num * (2**i))
   end
-  puts "Binary number #{b_string} is equal to decimal number #{dec_number}"
+  puts "Binary number #{arr.join} is equal to decimal number #{total}"
 end
 
 # 2. Write a method named `array_equals` that accepts two integer arrays as
@@ -24,7 +22,15 @@ end
 #       compare individual elements with each other and
 #       you may retrieve the length of an array.
 def array_equals(array1, array2)
-  puts "Not implemented"
+  if array1.length != array2.length
+    return false
+  else
+    array1.each_with_index do |num, i|
+      if num != array2[i]
+        return false
+      end
+    end
+  end
   return true
 end
 
@@ -32,46 +38,46 @@ end
 puts "Calling binary_to_decimal"
 binary_to_decimal
 
-# puts "Calling array_equals"
-# array1 = [10, 20, 30, 40, 50, 60]
-# array2 = [10, 20, 30, 40, 50, 60]
-# if !array_equals(array1, array2) # both equal case
-#   puts "BUG: array_equals should return true."
-#   print array1
-#   print array2
-# end
-#
-# array3 = [10, 20, 30, 40, 50, 60, 70] # not equal: first 6 elements same
-# if array_equals(array1, array3)
-#   puts "BUG: array_equals should return false."
-#   print array1
-#   print array3
-# end
-#
-# array4 = [20, 3, 50, 10, 68, 74] # not equal: same count case
-# if array_equals(array1, array4)
-#   puts "BUG: array_equals should return false."
-#   print array1
-#   print array4
-# end
-#
-# array5 = []
-# array6 = []
-# if !array_equals(array5, array6) # equal: empty array case
-#   puts "BUG: array_equals should return true."
-#   print array5
-#   print array6
-# end
-#
-# array7 = [20]
-# if array_equals(array4, array7) # not equal: one element, multiple elements case
-#   puts "BUG: array_equals should return false."
-#   print array4
-#   print array7
-# end
-#
-# if array_equals(array5, array7) # not equal: one empty, one not case
-#   puts "BUG: array_equals should return false."
-#   print array5
-#   print array7
-# end
+puts "Calling array_equals"
+array1 = [10, 20, 30, 40, 50, 60]
+array2 = [10, 20, 30, 40, 50, 60]
+if !array_equals(array1, array2) # both equal case
+  puts "BUG: array_equals should return true."
+  print array1
+  print array2
+end
+
+array3 = [10, 20, 30, 40, 50, 60, 70] # not equal: first 6 elements same
+if array_equals(array1, array3)
+  puts "BUG: array_equals should return false."
+  print array1
+  print array3
+end
+
+array4 = [20, 3, 50, 10, 68, 74] # not equal: same count case
+if array_equals(array1, array4)
+  puts "BUG: array_equals should return false."
+  print array1
+  print array4
+end
+
+array5 = []
+array6 = []
+if !array_equals(array5, array6) # equal: empty array case
+  puts "BUG: array_equals should return true."
+  print array5
+  print array6
+end
+
+array7 = [20]
+if array_equals(array4, array7) # not equal: one element, multiple elements case
+  puts "BUG: array_equals should return false."
+  print array4
+  print array7
+end
+
+if array_equals(array5, array7) # not equal: one empty, one not case
+  puts "BUG: array_equals should return false."
+  print array5
+  print array7
+end
