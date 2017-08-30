e6f94c2c7e3cfe4321fd6dc876b623b11aae7006
Irene DeVera <irenneeyyy@gmail.com>
2017-08-29 23:25:21 -0700

    Add binary_to_decimal method, array comparison method

--- binary_and_decimal.rb
+++ binary_and_decimal.rb
@@ -1,67 +1,77 @@
-# 1. Write a method named `binary_to_decimal` that creates an array of size 8.
-#    Fill the array randomly with 0’s and 1’s. Print out the array so that it
-#    appears as a binary number. Calculate the decimal value for this binary
-#    number using the algorithm you devised in class. Print out the decimal value.
 def binary_to_decimal
-  puts "Not implemented"
+  binary = Array.new(8) { rand(0..1) }
+  puts "Your binary list is: #{binary}"
+  total = 0
+  exp = 0
+  binary.reverse.each do |i|
+    total += i * 2 ** exp
+    exp += 1
+  end
+  puts "Your decimal value is: #{total}"
 end
 
-# 2. Write a method named `array_equals` that accepts two integer arrays as
-#    parameters. The method return `true` if the arrays contain the same elements
-#    in the same order, and returns `false` otherwise.
-# Note: Do not use Array class methods for comparing the whole array at once.
-#       You may use array indexing to retrieve one element at a time,
-#       compare individual elements with each other and
-#       you may retrieve the length of an array.
-def array_equals(array1, array2)
-  puts "Not implemented"
-  return true
+###
+
+def array_equals(array_1, array_2)
+  if array_1.length == array_2.length
+    # RUN BLOCK ONLY IF THE LENGTHS ARE THE SAME
+    array_1.each_with_index do |value, i|
+      # COMPARES INDEX VALUES OF EACH ARRAY
+      if value != array_2[i]
+        return false
+      end
+    end
+    return true # OUTSIDE OF LOOP TO RETURN ANYTHING NOT FALSE
+  else
+    return false # RETURN FALSE IF ARRAYS ARE NOT THE SAME LENGTH
+  end
 end
+
 
 # --- END OF METHODS ---
 puts "Calling binary_to_decimal"
 binary_to_decimal
 
 puts "Calling array_equals"
-array1 = [10, 20, 30, 40, 50, 60]
-array2 = [10, 20, 30, 40, 50, 60]
-if !array_equals(array1, array2) # both equal case
+array_1 = [10, 20, 30, 40, 50, 60]
+array_2 = [10, 20, 30, 40, 50, 60]
+if !array_equals(array_1, array_2) # both equal case
   puts "BUG: array_equals should return true."
-  print array1
-  print array2
+  print array_1
+  print array_2
 end
 
-array3 = [10, 20, 30, 40, 50, 60, 70] # not equal: first 6 elements same
-if array_equals(array1, array3)
+array_3 = [10, 20, 30, 40, 50, 60, 70] # not equal: first 6 elements same
+if array_equals(array_1, array_3)
   puts "BUG: array_equals should return false."
-  print array1
-  print array3
+  print array_1
+  print array_3
 end
 
-array4 = [20, 3, 50, 10, 68, 74] # not equal: same count case
-if array_equals(array1, array4)
+array_4 = [20, 3, 50, 10, 68, 74] # not equal: same count case
+if array_equals(array_1, array_4)
   puts "BUG: array_equals should return false."
-  print array1
-  print array4
+  print array_1
+  print array_4
 end
 
-array5 = []
-array6 = []
-if !array_equals(array5, array6) # equal: empty array case
+array_5 = []
+array_6 = []
+if !array_equals(array_5, array_6) # equal: empty array case
   puts "BUG: array_equals should return true."
-  print array5
-  print array6
+  print array_5
+  print array_6
 end
 
-array7 = [20]
-if array_equals(array4, array7) # not equal: one element, multiple elements case
+array_7 = [20]
+if array_equals(array_4, array_7) # not equal: one element, multiple elements case
   puts "BUG: array_equals should return false."
-  print array4
-  print array7
+  print array_4
+  print array_7
 end
 
-if array_equals(array5, array7) # not equal: one empty, one not case
+if array_equals(array_5, array_7) # not equal: one empty, one not case
   puts "BUG: array_equals should return false."
-  print array5
-  print array7
+  print array_5
+  print array_7
 end
