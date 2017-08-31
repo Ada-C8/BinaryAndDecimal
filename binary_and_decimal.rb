2766be6ef5d3cae3c5a453640736b8814f004396
hellomarisamorris <hellomarisamorris@gmail.com>
2017-08-30 21:08:20 -0700

    Methods complete & Tests passing.

--- binary_and_decimal.rb
+++ binary_and_decimal.rb
@@ -2,9 +2,30 @@
 #    Fill the array randomly with 0’s and 1’s. Print out the array so that it
 #    appears as a binary number. Calculate the decimal value for this binary
 #    number using the algorithm you devised in class. Print out the decimal value.
+
 def binary_to_decimal
-  puts "Not implemented"
+  # puts "Not implemented"
+  random = []
+  8.times do
+    random << [*0..1].sample
+  end
+
+  binary_num = random.join
+  random_reverse = random.reverse
+
+  decimal_value = 0
+  random_reverse.each_index { |index|
+    if (random_reverse[index] == 1)
+      decimal_value += 2 ** index
+    end
+  }
+
+  puts "Binary Number: #{binary_num}\nBinary converted to decimal: #{decimal_value}"
 end
+
+puts binary_to_decimal
+
+
 
 # 2. Write a method named `array_equals` that accepts two integer arrays as
 #    parameters. The method return `true` if the arrays contain the same elements
@@ -13,10 +34,28 @@ end
 #       You may use array indexing to retrieve one element at a time,
 #       compare individual elements with each other and
 #       you may retrieve the length of an array.
+# def array_equals(array1, array2)
+#   puts "Not implemented"
+#   return true
+# end
+
+
+# works for arrays of different lengths in addition to arrays of the same lengths
+
 def array_equals(array1, array2)
-  puts "Not implemented"
+  return false if (array1.length != array2.length)
+  array1.each_index { |index|
+    return false if (array1[index] != array2[index])
+  }
   return true
 end
+
+# array_1 = [1, 2, 3]
+# array_2 = [1, 2, 3, 4]
+#
+# puts array_equals(array_1, array_2)
+
+
 
 # --- END OF METHODS ---
 puts "Calling binary_to_decimal"
